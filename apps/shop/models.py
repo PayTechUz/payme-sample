56f8b5125421040fffcb6f707b31f189c6757954
Хусниддин <hoosnick000@gmail.com>
2023-08-25 19:13:36 +0500

    update models.py

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -4,5 +4,7 @@ from payme.models import BaseOrder
 
 
 class CustomOrderModel(BaseOrder):
-    user_id = models.IntegerField(null=True, blank=True)
-    # other fields...
+    user_id = models.IntegerField(null=True, blank=True) # other fields...
+
+    def __str__(self):
+        return f"ORDER ID: {self.id} - AMOUNT: {self.amount}"
