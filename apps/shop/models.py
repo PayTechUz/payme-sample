9f18ea3f48bb2865cae4b32e6341687caa173683
hoosnick <hoosnick000@gmail.com>
2023-08-26 17:17:14 +0500

    upd: to last payme-pkg-3.0b commit

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -4,7 +4,4 @@ from payme.models import BaseOrder
 
 
 class CustomOrderModel(BaseOrder):
-    user_id = models.IntegerField(null=True, blank=True) # other fields...
-
-    def __str__(self):
-        return f"ORDER ID: {self.id} - AMOUNT: {self.amount}"
+    user_id = models.IntegerField(null=True, blank=True)  # other fields...
